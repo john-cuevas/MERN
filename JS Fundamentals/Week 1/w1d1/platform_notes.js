9c60622132fed42cc503706c8c9817fa7f659f79
John Cuevas <jpmcuevas20@gmail.com>
2022-03-08 11:11:41 -0800

    platform notes

--- JS Fundamentals/Week 1/w1d1/platform_notes.js
+++ JS Fundamentals/Week 1/w1d1/platform_notes.js
@@ -80,67 +80,7 @@ let droneOnSale = drone - drone * 0.03;
 console.log(canAfford(drone, myBankAccountBalance));
 console.log(canAfford(droneOnSale, myBankAccountBalance));
 
-// classes
 
-class Vehicle{
-
-    // constructor always runs whenever we create a new instance
-    constructor(manufacturer, model, color) {
-        this.miles = 0;
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.color = color;
-    }
-
-    // method, function inside a class
-    drive(){
-        this.miles += 10;
-        console.log(
-            `You drove your ${ this.constructor.name } and it now has ${ this.miles } miles on it.`
-        );
-    }
-
-    // simple method in the parent class
-    parentFunction(){
-        return "This is coming from the parent!";
-    }
-}
-
-const car = new Vehicle ("BMW", "M5", "blue");
-car.drive();
-console.log(car.miles);
-console.log(car);
-
-// extends keyword - we can define new classes that inherit from existing classes
-
-// child M5 class
-
-class M5 extends Vehicle{
-    constructor(color){
-
-        // super allows us to call the constructor of the parent class
-        super("BMW", "M5", color);
-        this.hp = 616;
-    }
-
-    // simple function in the child class
-    childFunction(){
-
-        // by using super, we can call the parent method
-        const message = super.parentFunction();
-        console.log(message);
-    }
-    printSpecSummary(){
-        console.log(
-            `BMW M5 with a 4.4L V8 Twin Turbo engine packing ${this.hp}HP and 553 lb-ft TQ`
-        );
-    }
-}
-
-const m5 = new M5("yellow");
-m5.childFunction();
-
-console.log(m5)
 
 
 
