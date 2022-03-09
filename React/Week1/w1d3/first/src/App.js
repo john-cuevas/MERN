89a50c29267f0d209da995ffc0079c85b574e178
John Cuevas <jpmcuevas20@gmail.com>
2022-03-09 13:10:08 -0800

    first jsx

---
+++ React/Week1/w1d3/first/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
