22b36dc1fc3f322cfab6cdf2d1b681a6ed09ea94
John Cuevas <jpmcuevas20@gmail.com>
2022-03-09 13:45:25 -0800

    Prop it up

---
+++ React/Week1/w1d3/prop_it_up/src/components/PersonCard.jsx
@@ -0,0 +1,20 @@
+// import dependencies
+
+import React, { Component } from 'react'
+
+// initiate the class
+
+class PersonCard extends Component {
+    render() {
+        const {firstName, lastName, age, hairColor} = this.props
+        return (
+            <div>
+                <h1>{lastName}, {firstName}</h1> 
+                <p>Age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+            </div>
+        )
+    }
+}
+
+export default PersonCard
