328cdbeebe71230454ba84766a09205ef9e73e92
John Cuevas <jpmcuevas20@gmail.com>
2022-03-10 23:02:04 -0800

    more forms assignment

--- React/Week1/w1d4/hook_form/src/components/Form.jsx
+++ React/Week1/w1d4/hook_form/src/components/Form.jsx
@@ -8,63 +8,146 @@ const Form = () => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false)
-    
 
-    const createUser = (e) => {
-        e.preventDefault();
-        const newUser = { firstName, lastName, email, password }
-        console.log("Welcome", newUser)
-    }
+    const [firstNameError, setFirstNameError] = useState(true)
+    const [lastNameError, setLastNameError] = useState(true)
+    const [emailError, setEmailError] = useState(true)
+    const [passwordError, setPasswordError] = useState(true)
+    const [confirmPasswordError, setConfirmPasswordError] = useState(true)
 
-    const formMessage = () =>{
-        if (hasBeenSubmitted){
-            return ("Thank you for submitting the form");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevent refresh
+        if(!firstNameError && !lastNameError && !emailError && !passwordError && !confirmPasswordError){
+            const newUser = {firstName, lastName, email, password} // create user
+            console.log(newUser)
+            setHasBeenSubmitted(true)
         }
         else{
-            return("Welcome, please submit the form")
+            alert("error")
         }
     }
 
-    const handleName = (e) =>{
-        
+
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value)
+        if (e.target.value.length < 2 && e.target.value.length > 0) {
+            setFirstNameError("First Name must be at least 2 characters")
+        }
+        else {
+            setFirstNameError("")
+        }
+
+    }
+    const handleLastName = (e) => {
+        setLastName(e.target.value)
+        if (e.target.value.length < 2 && e.target.value.length > 0) {
+            setLastNameError("Last Name must be at least 2 characters")
+        }
+        else {
+            setLastNameError("")
+        }
+
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+        if (e.target.value.length < 2 && e.target.value.length > 0) {
+            setEmailError("Email must be at least 2 characters")
+        }
+        else {
+            setEmailError("")
+        }
+
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 8 && e.target.value.length > 0) {
+            setPasswordError("Password must be at least 8 characters")
+        }
+        else {
+            setPasswordError("")
+        }
+
+    }
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value)
+        if (e.target.value !== password) {
+            setConfirmPasswordError("Passwords must match")
+        }
+        else {
+            setConfirmPasswordError("")
+        }
+
     }
 
     return (
         <div>
 
-        <form onSubmit={createUser}>
-            <div>
-                <label> First Name:</label>
-                <input type="text" onChange={(e) => setFirstName(e.target.value)} />
-            </div>
-            <div>
-                <label> Last Name:</label>
-                <input type="text" onChange={(e) => setLastName(e.target.value)} />
-            </div>
-            <div>
-                <label> Email:</label>
-                <input type="text" onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div>
+            {
+                hasBeenSubmitted?
+                <h1>Thank you for submitting the form</h1>:
+                <h1>Please fill in the form</h1>
+            }
 
-                <label> Password:</label>
-                <input type="text" onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label> First Name:</label>
+                    <input type="text" name="firstName" onChange={handleFirstName} value={firstName} />
+                    {
+                        firstNameError ?
+                            <p>{firstNameError}</p> :
+                            ""
+                }
+                </div>
+                <div>
+                    <label> Last Name:</label>
+                    <input type="text" name="lastName" onChange={handleLastName} value={lastName} />
+                    {
+                        lastNameError ?
+                            <p>{lastNameError}</p> :
+                            ""
+                }
+                </div>
+                <div>
+                    <label> Email:</label>
+                    <input type="text" name="email" onChange={handleEmail} value={email} />
+                    {
+                        emailError ?
+                            <p>{emailError}</p> :
+                            ""
+                }
+                </div>
+                <div>
 
-                <label> Confirm Password:</label>
-                <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} />
-            </div>
-        </form>
+                    <label> Password:</label>
+                    <input type="password" name="password" onChange={handlePassword} value={password} />
+                    {
+                        passwordError ?
+                            <p>{passwordError}</p> :
+                            ""
+                }
+                </div>
+                <div>
 
-        <h3>Your Form Data</h3>
-        <p>First Name: {firstName}</p>
-        <p>Last Name: {lastName}</p>
-        <p>Email: {email}</p>
-        <p>Password: {password}</p>
-        <p>Confirm Password: {confirmPassword}</p>
+                    <label> Confirm Password:</label>
+                    <input type="confirmPassword" name="confirmPassword" onChange={handleConfirmPassword} value={confirmPassword} />
+                    {
+                        confirmPasswordError ?
+                            <p>{confirmPasswordError}</p> :
+                            ""
+                }
+                </div>
+                <button disabled = {firstNameError || lastNameError || emailError || confirmPasswordError}>Submit</button>
+            </form>
+
+            <h3>Your Form Data</h3>
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+            <p>Confirm Password: {confirmPassword}</p>
         </div>
-            
+
     )
 }
 
